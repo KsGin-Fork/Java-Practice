0b1092448773589b6a28dd66a0e0effe3bb4013b
帆 杨 <imqqyangfan@outlook.com>
2016-10-19 23:32:04 +0800

    bug

--- src/homework/J07/C11_5.java
+++ src/homework/J07/C11_5.java
@@ -7,13 +7,20 @@ import java.util.ArrayList;
  */
 public class C11_5 {
     public static void main(String[] args){
-
+        Course course = new Course("数据结构");
+        course.addStudent("杨帆");
+        course.addStudent("李一斐");
+        System.out.println(course.getCourseName() + "课程学生为:");
+        String[] a = course.getStudents();
+        for (int i = 0 ; i < course.getNumberOfStudents() ; i++){
+            System.out.println(a[i]);
+        }
     }
 }
 
 class Course{
     private String courseName;
-    private ArrayList<String> students;
+    private ArrayList<String> students = new ArrayList<>();
     public Course(String courseName){
         this.courseName = courseName;
     }
