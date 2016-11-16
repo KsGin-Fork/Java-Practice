386154c26fed9ea15c35abefae21df92be4be0db
帆 杨 <imqqyangfan@outlook.com>
2016-11-16 22:13:33 +0800

    add class of GUI

---
+++ src/homework/J11/G16_16.java
@@ -0,0 +1,35 @@
+package homework.J11;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.TimerTask;
+
+/**
+ * Created by YangFan on 2016/11/16.
+ */
+public class G16_16{
+    public static void main(String[] args){
+        Show s = new Show();
+    }
+}
+
+class Show extends JFrame{
+    boolean flag = true;
+    JLabel jLabel = new JLabel("www.xxxxxx.com");
+    public Show(){
+        jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        jLabel.setVerticalAlignment(SwingConstants.CENTER);
+        this.add(jLabel);
+        this.setSize(500,250);
+        this.setVisible(true);
+        java.util.Timer timer = new java.util.Timer(true);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                flag = !flag;
+                jLabel.setVisible(flag);
+            }
+        };
+        timer.schedule(task,1000 ,300);
+    }
+}
