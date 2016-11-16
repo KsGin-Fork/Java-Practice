8701b06e388d0ff378cf228b83083082253956c8
帆 杨 <imqqyangfan@outlook.com>
2016-11-16 21:02:42 +0800

    add class of GUI

---
+++ src/homework/J11/G16_8.java
@@ -0,0 +1,57 @@
+package homework.J11;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+
+/**
+ * Created by YangFan on 2016/11/16.
+ */
+public class G16_8 {
+    public static void main(String[] args){
+        MouseDemo ms = new MouseDemo();
+    }
+}
+
+class MouseDemo extends JFrame{
+    JLabel outPutLabel = new JLabel();
+    public MouseDemo(){
+        this.setLayout(null);
+        this.setSize(500,250);
+        this.setVisible(true);
+        this.add(outPutLabel);
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                outPutLabel.setBounds(e.getX() , e.getY()-40 , 100 ,20);
+                outPutLabel.setText("(" + e.getX() + "," + e.getY() + ")");
+                outPutLabel.setVisible(true);
+            }
+        });
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                outPutLabel.setBounds(e.getX() , e.getY()-40 , 100 ,20);
+                outPutLabel.setText("(" + e.getX() + "," + e.getY() + ")");
+                outPutLabel.setVisible(true);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                outPutLabel.setVisible(false);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                outPutLabel.setVisible(false);
+            }
+
+
+        });
+    }
+
+
+}
